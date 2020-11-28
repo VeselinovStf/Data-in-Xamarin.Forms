64c7ae01e2a201b5a7d1b70e4a3c4bcba8a4e084
s v <chofexx@gmail.com>
2020-11-28 09:35:47 +0200

    Adding WCF Service

---
+++ src/Services/BethanysPieShopStock.Services.WcfService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BethanysPieShopStock.Services.WcfService.Global" Language="C#" %>
---
+++ src/Services/BethanysPieShopStock.Services.WcfService/Models/IPieRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BethanysPieShopStock.Services.WcfService.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+        Pie GetPieById(Guid id);
+        void AddPie(Pie pie);
+        void UpdatePie(Pie pie);
+        void DeletePie(Guid id);
+    }
+}
